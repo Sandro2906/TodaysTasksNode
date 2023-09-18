f7edfaa1e753d906710b62b98a57699c69aee0a9
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-18 02:03:37 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://todays-tasks-node.vercel.app',
     credentials: true
 }))
 
